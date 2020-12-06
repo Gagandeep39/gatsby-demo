feb84e310ba952d4a679d51ece8072a229c2f4d6
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-06 09:02:30 +0000

    Created blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-06 09:02:12
+ * @modify date 2020-12-06 09:02:12
+ * @desc Blog Page
+ */
+import React from "react"
+
+export default function blog() {
+  return <div>Blog Page</div>
+}
