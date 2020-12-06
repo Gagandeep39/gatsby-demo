3ffaabbe2321d7788539bfee382c30d1236a0a46
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-06 09:08:42 +0000

    Added routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const index = () => {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Hi! I am a full stack developer</h1>
+      <p>
+        <Link to="/contact">Contact Us</Link>
+      </p>
+      <p>
+        <Link to="/about">About Us</Link>
+      </p>
+    </div>
+  )
 }
 
 export default index
