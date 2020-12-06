b03984aee9b31d82f602afa53e0bce1c8b763da0
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-06 09:27:32 +0000

    Created layout page

---
+++ src/components/layout.js
@@ -0,0 +1,22 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-06 09:23:03
+ * @modify date 2020-12-06 09:23:03
+ * @desc Layout
+ */
+import React from "react"
+import Header from "./header"
+import Footer from "./footer"
+
+const Layout = props => {
+  return (
+    <div>
+      <Header />
+      {props.children}
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
