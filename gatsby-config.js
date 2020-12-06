7dd8b206568475b533111b20b24600ae19dafd8a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-06 13:08:38 +0000

    Added meta data in config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Full-stack Gatsby App',
+    author: 'Gagandeep Singh'
+  },
   plugins: [
     `gatsby-plugin-sass`
   ],
