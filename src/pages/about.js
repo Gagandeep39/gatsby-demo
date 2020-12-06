9c32f8b638655bf00954349c41610057486e95af
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-06 09:03:51 +0000

    Added about and contacts page

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-06 09:03:25
+ * @modify date 2020-12-06 09:03:25
+ * @desc About page
+ */
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>
+      About Pags
+    </div>
+  )
+}
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-06 09:03:09
+ * @modify date 2020-12-06 09:03:09
+ * @desc Contact page
+ */
+import React from "react"
+
+export default function contact() {
+  return <div>Contacts Page</div>
+}
