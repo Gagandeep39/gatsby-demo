e1177cbd21d1d3bacb59d4b35081ead71e260cb7
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-06 19:09:04 +0000

    Added logic to fetch data in blog

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -5,13 +5,40 @@
  * @modify date 2020-12-06 22:17:00
  * @desc Home Page
  */
+import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../components/layout'
 
-const Blog = () => {
+// Gtsy will actualy fetch this query
+// It will run it and put the response in props
+export const query = graphql`
+query (
+    $slug: String!
+) {
+    markdownRemark (
+        fields: {
+        slug: {
+            eq: $slug
+        }
+    }
+    ) {
+        frontmatter {
+            title
+            date
+        }
+        html
+    }
+}
+`
+
+const Blog = (props) => {
     return (
         <Layout>
-            Markdown Content
+            <h1> {props.data.markdownRemark.frontmatter.title} </h1>
+            <p> {props.data.markdownRemark.frontmatter.date} </p>
+            <hr />
+            {/* Render HTML */}
+            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
         </Layout>
     )
 }
