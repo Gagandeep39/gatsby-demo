750abb1ea1f2c260d666285ae45d791af584042b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-07 05:37:23 +0000

    Removed unused content

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,24 @@
 const path = require('path')
 
-exports.onCreateNode = ({ node, actions }) => {
-    const { createNode, createNodeField } = actions
-    // Transform the new node here and create a new node or
-    // create a new node field.
-    // See all nodes
-    // Lock for JSON structure to find node in which we have to add a new field
-    // console.log(JSON.stringify(node, undefined, 4));
-    if (node.internal.type === 'MarkdownRemark') {
-        const slug = path.basename(node.fileAbsolutePath, '.md')
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        })
-    }
-}
+//////////////////////////////////////////////////////////
+// Not needed any more, as we are fething data from server
+//////////////////////////////////////////////////////////
+// exports.onCreateNode = ({ node, actions }) => {
+//     const { createNode, createNodeField } = actions
+//     // Transform the new node here and create a new node or
+//     // create a new node field.
+//     // See all nodes
+//     // Lock for JSON structure to find node in which we have to add a new field
+//     // console.log(JSON.stringify(node, undefined, 4));
+//     if (node.internal.type === 'MarkdownRemark') {
+//         const slug = path.basename(node.fileAbsolutePath, '.md')
+//         createNodeField({
+//             node,
+//             name: 'slug',
+//             value: slug
+//         })
+//     }
+// }
 
 // Create dynamic routes with as /blog/<mdfile name>
 exports.createPages = async ({ graphql, actions }) => {
