cfc34f963ea7e4ef19487dfe7d8b615cb709b21d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-07 07:36:37 +0000

    Fixed a typo

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -35,8 +35,8 @@ const Blog = (props) => {
     const options = {
         renderNode: {
             "embedded-asset-block": (node) => {
-                const alt = node.dat.target.fields.title['en-US'] ||''
-                const url = node.dat.target.fields.file['en-US'].url ||''
+                const alt = node.data.target.fields.title['en-US'] ||''
+                const url = node.data.target.fields.file['en-US'].url ||''
                 return <img alt={alt} src={url} />
             }
         }
