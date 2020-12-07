d655f32db45999547e3426dceb6e4ae1ee522a57
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-07 07:20:24 +0000

    Added image support

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -31,11 +31,23 @@ query (
 `
 
 const Blog = (props) => {
+
+    const options = {
+        renderNode: {
+            "embedded-asset-block": (node) => {
+                const alt = node.dat.target.fields.title['en-US'] ||''
+                const url = node.dat.target.fields.file['en-US'].url ||''
+                return <img alt={alt} src={url} />
+            }
+        }
+    }
+
     return (
         <Layout>
             <h1> {props.data.contentfulBlogPost.title} </h1>
             <p> {props.data.contentfulBlogPost.publishedDate} </p>
-            {documentToReactComponents(props.data.contentfulBlogPost.body.json)}
+            {/* Convert json to markdown */}
+            {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
         </Layout>
     )
 }
